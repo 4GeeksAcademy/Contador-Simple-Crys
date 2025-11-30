bbe0e9be7e5510d27f96ee4a29810ba402f8c14b
Crys Carmona <crysc4rmon4@gmail.com>
2025-11-30 10:00:47 +0000

    primeros archivo yfuncion SecondsCounter

---
+++ src/js/components/SecondsCounter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import React, { useState, useEffect } from "react";
+/**
+ * Componente SecondsCounter
+ * De momento es una maqueta vacía.
+ * Luego le inyectaremos la lógica de conteo.
+ */
+export default function SecondsCounter() {
+  return (
+   const [numeroActual, setNumeroActual] = useState(0)
+  );
+}
